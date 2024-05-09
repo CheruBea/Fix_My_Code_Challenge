0290d3271285b4d2f8b1d36b4a4807f48d10b021
CheruBea <bettybellann@gmail.com>
2024-05-09 10:23:52 +0300

    square fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -14,11 +14,10 @@ class Square:
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return f"{self.width}/{self.height}"
+        return f"Square: width={self.width}, height={self.height}"
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
-
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
